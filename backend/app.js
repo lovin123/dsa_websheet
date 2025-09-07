fb93e6da6a542b17f968ee17cd86298e8316ad35
lovintanwar123 <lovin.tanwar@kollegeapply.com>
2025-09-07 20:37:30 +0530

    cors error

--- backend/app.js
+++ backend/app.js
@@ -7,16 +7,12 @@ const connectDB = require("./config/mongo");
 
 const path = require("path");
 const app = express();
-app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
+app.use(
+  cors({
+    origin: "*", // allows all origins
+    credentials: true, // allows cookies/auth headers
+  })
+);
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
